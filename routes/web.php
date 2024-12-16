00bf3ec36df8deeace2cd0c5c16334c3c26e1913
Grishma Giru <grishmashrestha12345@gmail.com>
2024-12-16 07:58:03 +0545

    Normalize line endings

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Homecontroller;
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
 
-sdfhskjfhs
+route::get('/redirect',[HomeController::class,'redirect']);
